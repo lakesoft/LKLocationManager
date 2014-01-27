d079daa5078f2ccea5a076d612ee803a9ac70951
Hiroshi Hashiguchi <hashiguchi@lakesoft.jp>
2014-01-27 18:26:38 +0900

    Initial Commit

---
+++ LKLocationManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LKLocationManager
+//
+//  Created by Hiroshi Hashiguchi on 2014/01/27.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LKLocationManagerTests/LKLocationManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  LKLocationManagerTests.m
+//  LKLocationManagerTests
+//
+//  Created by Hiroshi Hashiguchi on 2014/01/27.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LKLocationManagerTests : XCTestCase
+
+@end
+
+@implementation LKLocationManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
